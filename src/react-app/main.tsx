ca9f75f8d7df109693a8391d230be2c11d8cfcb1
saridsa2 <s.satya.suman@globaluniversityfoundation.com>
2025-05-09 17:54:43 +0530

    Initial Commit

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,18 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+function AppWithTitle() {
+  React.useEffect(() => {
+    document.title = "Operation Sindhoor";
+  }, []);
+  return <App />;
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppWithTitle />
   </StrictMode>,
 );
