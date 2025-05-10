3e68b8a29f74fe0d3ff2dc2a85e8166643f3a4fc
saridsa2 <s.satya.suman@globaluniversityfoundation.com>
2025-05-10 07:21:41 +0530

    Changes 1

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,11 +1,50 @@
 // src/App.tsx
+import React, { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+  const allowedSequence = ["j", "a", "i"];
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.toLowerCase();
+    let newValue = "";
+    for (let i = 0; i < value.length && i < 3; i++) {
+      if (value[i] === allowedSequence[i]) {
+        newValue += value[i];
+      } else {
+        break;
+      }
+    }
+    setInputValue(newValue.toUpperCase());
+  };
+
+  const handleKeyDown = () => {};
+
+  const handleBlur = () => {};
+
   return (
     <div>
       <img src="/GqURL0BXUAApbgD.jpeg" alt="Operation Sindoor" className="fullscreen-image" />
-      <div className="footer-text">operation underway..</div>
+      <div className="overlay-army">
+        <div className="army-text">Operation Sindhoor Underway</div>
+        <div className="army-subtext">
+          Bharath Mata Ki..{' '}
+          <input
+            ref={inputRef}
+            className="army-input"
+            type="text"
+            value={inputValue}
+            onChange={handleInput}
+            onKeyDown={handleKeyDown}
+            onBlur={handleBlur}
+            maxLength={3}
+            autoComplete="off"
+          />
+        </div>
+      </div>
     </div>
   );
 }
